35d042293ca3975038ce4f6c6fd0397fad919b3a
Puyan Wei <puyanwei@gmail.com>
2018-06-11 13:37:22 +0100

    Started again using a different direction, wrote passing test for ES6 class

--- test/coin-changer-test.js
+++ test/coin-changer-test.js
@@ -1,29 +1,16 @@
-describe('#returnCoins', () => {
-  it('Returns correct the number of coins in a string', () => {
-    expect(returnCoins(75, 25)).toEqual(3);
-    expect(returnCoins(63, 25)).toEqual(2);
-    expect(returnCoins(70, 10)).toEqual(7);
-    expect(returnCoins(63, 10)).toEqual(6);
-    expect(returnCoins(-33, 25)).toEqual(0);
-    expect(returnCoins(-33, 10)).toEqual(0);
+describe('#CoinChanger', () => {
+  let coinChanger;
+  beforeEach(() => {
+    coinChanger = new CoinChanger();
   });
-});
 
-describe('#returnChange', () => {
-  it('Returns remainder if there is any', () => {
-    expect(returnChange(100, 23)).toEqual(8);
-    expect(returnChange(75, 25)).toEqual(0);
-  });
-});
-
-describe('#convertToString', () => {
-  it('Converts the number into string based on number', () => {
-    expect(convertToString(4, 'Q')).toEqual('QQQQ');
-  });
-});
-
-describe('#controller', () => {
-  it('Converts the number into string based on number', () => {
-    expect(controller(100).toEqual('QQQQ');
+  describe('#initialise', () => {
+    it('till has object by default', () => {
+      expect(coinChanger.till).toEqual({
+        25: 0,
+        10: 0,
+        1: 0,
+      });
+    });
   });
 });
