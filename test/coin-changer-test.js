4786141fd712a8a53dc97f024165dc7fece65a54
Puyan Wei <puyanwei@gmail.com>
2018-06-11 11:30:19 +0100

    Failing test for #returnChange

--- test/coin-changer-test.js
+++ test/coin-changer-test.js
@@ -1,7 +1,12 @@
-describe('Gives the correct change', () => {
+describe('#convertToQuarters', () => {
   it('Returns correct the number of quarters', () => {
     expect(convertToQuarters(75)).toEqual(3);
     expect(convertToQuarters(63)).toEqual(2);
     expect(convertToQuarters(-33)).toEqual(0);
+  });
+});
+describe('#returnChange', () => {
+  it('Returns remainder if there is any', () => {
+    expect(returnChange(100, 23).toEqual(8));
   });
 });
