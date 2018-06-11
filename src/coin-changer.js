0f1ad3d3f56db2f7fd276805468d136b00370a77
Puyan Wei <puyanwei@gmail.com>
2018-06-11 17:08:04 +0100

    #returnCoins returns the correct amount of coins represented by letters Q, D, and P

--- src/coin-changer.js
+++ src/coin-changer.js
@@ -5,17 +5,26 @@ class CoinChanger {
   }
 
   quarters() {
-    this.till[25] = this.amount / 25;
-    this.amount = Math.floor(this.amount % 25);
+    this.till[25] = 'Q'.repeat(Math.floor(this.amount / 25));
+    this.amount = this.amount % 25;
   }
 
   dimes() {
-    this.till[10] = this.amount / 10;
-    this.amount = Math.floor(this.amount % 10);
+    this.till[10] = 'D'.repeat(Math.floor(this.amount / 10));
+    this.amount = this.amount % 10;
   }
 
   pennies() {
-    this.till[1] = this.amount / 1;
-    this.amount = Math.floor(this.amount % 1);
+    this.till[1] = 'P'.repeat(Math.floor(this.amount / 1));
+    this.amount = this.amount % 1;
+  }
+
+  returnCoins() {
+    this.quarters();
+    this.dimes();
+    this.pennies();
+    let string = this.till[25].concat(this.till[10]).concat(this.till[1]);
+    return string;
+    console.log(this.till, this.amount);
   }
 }
--- test/coin-changer-test.js
+++ test/coin-changer-test.js
@@ -1,35 +1,36 @@
 describe('#CoinChanger', () => {
   beforeEach(() => {
-    coinChanger = new CoinChanger(100);
+    coinChanger = new CoinChanger(39);
   });
 
   describe('#initialise', () => {
     it('till has object by default', () => {
-      expect(coinChanger.amount).toEqual(100);
+      expect(coinChanger.amount).toEqual(39);
       expect(coinChanger.till).toEqual({ 25: 0, 10: 0, 1: 0 });
     });
   });
 
   describe('#quarters', () => {
-    it('Adds number of quarters to the till, and updates the amount left', () => {
+    it('Adds number of quarters to the till by the letter Q, and updates the amount left', () => {
       expect(coinChanger.quarters());
-      expect(coinChanger.till).toEqual({ 25: 4, 10: 0, 1: 0 });
-      expect(coinChanger.amount).toEqual(0);
+      expect(coinChanger.till).toEqual({ 25: 'Q', 10: 0, 1: 0 });
+      expect(coinChanger.amount).toEqual(14);
     });
   });
 
   describe('#dimes', () => {
-    it('Adds number of dimes to the till, and updates the amount left', () => {
+    it('Adds number of dimes to the till by the letter D, and updates the amount left', () => {
       expect(coinChanger.dimes());
-      expect(coinChanger.till).toEqual({ 25: 0, 10: 10, 1: 0 });
-      expect(coinChanger.amount).toEqual(0);
+      expect(coinChanger.till).toEqual({ 25: 0, 10: 'DDD', 1: 0 });
+      expect(coinChanger.amount).toEqual(9);
     });
   });
 
   describe('#pennies', () => {
     it('Adds number of pennies to the till, and updates the amount left', () => {
+      coinChanger.amount = 4;
       expect(coinChanger.pennies());
-      expect(coinChanger.till).toEqual({ 25: 0, 10: 0, 1: 100 });
+      expect(coinChanger.till).toEqual({ 25: 0, 10: 0, 1: 'PPPP' });
       expect(coinChanger.amount).toEqual(0);
     });
   });
