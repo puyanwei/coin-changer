a08933235aca04c2e3d12786539ef917e9c35beb
Puyan Wei <puyanwei@gmail.com>
2018-06-11 11:35:54 +0100

    Passing test for #convertToDimes

--- src/coin-changer.js
+++ src/coin-changer.js
@@ -5,6 +5,13 @@ function convertToQuarters(num) {
     return Math.floor(num / 25);
   }
 }
+function convertToDimes(num) {
+  if (num < 0) {
+    return 0;
+  } else {
+    return Math.floor(num / 10);
+  }
+}
 
 function returnChange(numerator, denominator) {
   return numerator % denominator;
