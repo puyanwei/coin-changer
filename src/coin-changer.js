445221761fc5ce6ca5e3d950f8a2d3bf1beaa21b
Puyan Wei <puyanwei@gmail.com>
2018-06-11 13:40:41 +0100

    Amount added to class as default

---
+++ src/coin-changer.js
@@ -0,0 +1,10 @@
+class CoinChanger {
+  constructor(amount) {
+    this.amount = amount;
+    this.till = {
+      25: 0,
+      10: 0,
+      1: 0,
+    };
+  }
+}
--- test/coin-changer-test.js
+++ test/coin-changer-test.js
@@ -1,11 +1,11 @@
 describe('#CoinChanger', () => {
-  let coinChanger;
   beforeEach(() => {
-    coinChanger = new CoinChanger();
+    coinChanger = new CoinChanger(100);
   });
 
   describe('#initialise', () => {
     it('till has object by default', () => {
+      expect(coinChanger.amount).toEqual(100);
       expect(coinChanger.till).toEqual({
         25: 0,
         10: 0,
