d8099376205f0f45857343c58c566dc09c252847
Puyan Wei <puyanwei@gmail.com>
2018-06-13 11:27:12 +0100

    Wrote tests for all denominators for #addToTillAndUpdateAmount

--- test/coin-changer-test.js
+++ test/coin-changer-test.js
@@ -30,44 +30,71 @@ describe('#CoinChanger', () => {
             });
             expect(coinChanger.amount).toEqual(14);
         });
-    });
-    it('Adds number of fifties to the till array, and updates the amount left', () => {
-        coinChanger.addToTillAndUpdateAmount(50);
-        expect(coinChanger.till).toEqual({
-            100: [],
-            50: [50, 50, 50, 50, 50, 50],
-            20: [],
-            10: [],
-            5: [],
-            1: [],
+        it('Adds number of fifties to the till array, and updates the amount left', () => {
+            coinChanger.addToTillAndUpdateAmount(50);
+            expect(coinChanger.till).toEqual({
+                100: [],
+                50: [50, 50, 50, 50, 50, 50],
+                20: [],
+                10: [],
+                5: [],
+                1: [],
+            });
+            expect(coinChanger.amount).toEqual(14);
         });
-        expect(coinChanger.amount).toEqual(14);
-    });
-    it('Adds number of twenties to the till array, and updates the amount left', () => {
-        coinChanger.amount = 45;
-        coinChanger.addToTillAndUpdateAmount(20);
-        expect(coinChanger.till).toEqual({
-            100: [],
-            50: [],
-            20: [20, 20],
-            10: [],
-            5: [],
-            1: [],
+        it('Adds number of twenties to the till array, and updates the amount left', () => {
+            coinChanger.amount = 45;
+            coinChanger.addToTillAndUpdateAmount(20);
+            expect(coinChanger.till).toEqual({
+                100: [],
+                50: [],
+                20: [20, 20],
+                10: [],
+                5: [],
+                1: [],
+            });
+            expect(coinChanger.amount).toEqual(5);
         });
-        expect(coinChanger.amount).toEqual(5);
+        it('Adds number of tens to the till array, and updates the amount left', () => {
+            coinChanger.amount = 45;
+            coinChanger.addToTillAndUpdateAmount(10);
+            expect(coinChanger.till).toEqual({
+                100: [],
+                50: [],
+                20: [],
+                10: [10, 10, 10, 10],
+                5: [],
+                1: [],
+            });
+            expect(coinChanger.amount).toEqual(5);
+        });
+        it('Adds number of fives to the till array, and updates the amount left', () => {
+            coinChanger.amount = 17;
+            coinChanger.addToTillAndUpdateAmount(5);
+            expect(coinChanger.till).toEqual({
+                100: [],
+                50: [],
+                20: [],
+                10: [],
+                5: [5, 5, 5],
+                1: [],
+            });
+            expect(coinChanger.amount).toEqual(2);
+        });
+        it('Adds number of ones to the till array, and updates the amount left', () => {
+            coinChanger.amount = 4;
+            coinChanger.addToTillAndUpdateAmount(1);
+            expect(coinChanger.till).toEqual({
+                100: [],
+                50: [],
+                20: [],
+                10: [],
+                5: [],
+                1: [1, 1, 1, 1],
+            });
+            expect(coinChanger.amount).toEqual(0);
+        });
     });
-    // it('Adds number of fifties to the till array, and updates the amount left', () => {
-    //     coinChanger.addToTillAndUpdateAmount(50);
-    //     expect(coinChanger.till).toEqual({
-    //         100: [],
-    //         50: [50, 50, 50, 50, 50, 50],
-    //         20: [],
-    //         10: [],
-    //         5: [],
-    //         1: [],
-    //     });
-    //     expect(coinChanger.amount).toEqual(14);
-    // });
     // describe('#returnCoins', () => {
     //   it('Returns the correct amount of coins in a string', () => {
     //     coinChanger.amount = 89;
