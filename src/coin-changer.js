8335965f17a23cb95cf2d86ce52e11a8bca617e9
Puyan Wei <puyanwei@gmail.com>
2018-06-13 10:54:29 +0100

    Test passing to till change

--- src/coin-changer.js
+++ src/coin-changer.js
@@ -1,19 +1,25 @@
 class CoinChanger {
-  constructor(amount) {
-    this.amount = amount;
-    this.till = { 25: 0, 10: 0, 1: 0 };
-  }
+    constructor(amount) {
+        this.amount = amount;
+        this.till = {
+            100: [],
+            50: [],
+            25: [],
+            10: [],
+            1: [],
+        };
+    }
 
-  addToTillAndUpdateAmount(coin, letter) {
-    this.till[coin] = letter.repeat(Math.floor(this.amount / coin));
-    this.amount = this.amount % coin;
-  }
+    addToTillAndUpdateAmount(coin, letter) {
+        this.till[coin] = letter.repeat(Math.floor(this.amount / coin));
+        this.amount = this.amount % coin;
+    }
 
-  returnCoins() {
-    this.addToTillAndUpdateAmount(25, 'Q');
-    this.addToTillAndUpdateAmount(10, 'D');
-    this.addToTillAndUpdateAmount(1, 'P');
-    let string = this.till[25].concat(this.till[10]).concat(this.till[1]);
-    return string;
-  }
+    returnCoins() {
+        this.addToTillAndUpdateAmount(25, 'Q');
+        this.addToTillAndUpdateAmount(10, 'D');
+        this.addToTillAndUpdateAmount(1, 'P');
+        let string = this.till[25].concat(this.till[10]).concat(this.till[1]);
+        return string;
+    }
 }
